3e1ec4d1db6fb6e3ab7083f2b818af0e8a0c956f
tonydefaria <me@tonydefaria.com>
2026-01-11 16:39:00 +0100

    text next.config.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "zeimoto.com" }],
+        destination: "https://www.zeimoto.com/:path*",
+        statusCode: 301,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
